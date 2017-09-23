99da490eb90c6876a284d580f816cdfe5da8646d
Mikhail <misha.rudanov@yandex.ru>
2017-09-23 23:51:02 +0300

    Initial Commit

---
+++ Fintech Chat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fintech Chat
+//
+//  Created by Mikhail Rudanov on 23/09/2017.
+//  Copyright © 2017 Mikhail Rudanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
